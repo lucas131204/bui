832e4497ce1998392eb41a84bec7ff03518e02ff
lucas131204 <72490661+lucas131204@users.noreply.github.com>
2021-01-09 16:31:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const Discord = require('discord.js');
 const config = require("./config.json")
 const client = new Discord.Client();
 
+client.on('message', message => {
+    let args = message.content.split(/ +/g);
+    let cmd  = args.shift().toLowerCase();
+    if(cmd === `${prefix}repeat`){
+        message.channel.send(args.join(" "));
+        message.delete({ timeout: 3000}).then(message => console.log("le message :" + message.content));
+    }
+    
+});
+
 client.on('ready', () => {
     console.log('je suis trop baux');
 });
